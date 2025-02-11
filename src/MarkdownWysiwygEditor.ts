3408eef99080345a768e51b826316ef0f21309f1
yjgaia <yjdev88@gmail.com>
2025-02-11 22:16:46 +0900

    Make editor area content editable in MarkdownWysiwygEditor class

--- src/MarkdownWysiwygEditor.ts
+++ src/MarkdownWysiwygEditor.ts
@@ -7,6 +7,7 @@ export default class MarkdownWysiwygEditor extends DomNode {
     super(".markdown-wysiwyg-editor");
 
     this.editorArea = el(".editor-area");
+    this.editorArea.htmlElement.contentEditable = "true";
 
     this.append(el(".toolbar"), this.editorArea);
   }
