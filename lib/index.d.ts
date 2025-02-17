0786bed54c86ad45ba325c3d3cd42228ca5bec16
yjgaia <yjdev88@gmail.com>
2025-02-17 12:34:03 +0900

    Export MarkdownWysiwygEditor from index files for improved module accessibility

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,2 @@
+export { default as MarkdownWysiwygEditor } from "./MarkdownWysiwygEditor.js";
 //# sourceMappingURL=index.d.ts.map
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as MarkdownWysiwygEditor } from "./MarkdownWysiwygEditor.js";
