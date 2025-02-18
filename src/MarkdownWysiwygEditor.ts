9abe9e4d031485b33a07a489b32d39b951ba08de
yjgaia <yjdev88@gmail.com>
2025-02-18 11:37:42 +0900

    Refactor MarkdownWysiwygEditor to use MarkdownLexer, MarkdownParser, and MarkdownHTMLCompiler for improved markdown processing

--- src/MarkdownWysiwygEditor.ts
+++ src/MarkdownWysiwygEditor.ts
@@ -1,5 +1,9 @@
 import { DomNode, el } from "@common-module/app";
-import { MarkdownHtmlConverter } from "@common-module/markdown";
+import {
+  MarkdownHTMLCompiler,
+  MarkdownLexer,
+  MarkdownParser,
+} from "@common-module/markdown";
 import { Debouncer } from "@common-module/ts";
 
 export default class MarkdownWysiwygEditor extends DomNode {
@@ -19,8 +23,12 @@ export default class MarkdownWysiwygEditor extends DomNode {
   }
 
   private onInput(): void {
-    const rawText = this.editorArea.htmlElement.innerText;
-    const html = MarkdownHtmlConverter.convertMarkdownToHtml(rawText);
+    const markdownText = this.editorArea.htmlElement.innerText;
+    const tokens = MarkdownLexer.tokenize(markdownText);
+    console.log(tokens);
+    const ast = new MarkdownParser(tokens).parse();
+    console.log(ast);
+    const html = MarkdownHTMLCompiler.compile(ast);
     console.log(html);
   }
 }
