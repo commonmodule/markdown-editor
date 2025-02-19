ec7a3fae7bf6f3d408e6572eef2a6d244ba3b3d0
yjgaia <yjdev88@gmail.com>
2025-02-19 14:04:15 +0900

    Add caret management and Markdown parsing features to MarkdownEditor

--- lib/src/MarkdownEditor.d.ts
+++ lib/src/MarkdownEditor.d.ts
@@ -3,5 +3,9 @@ export default class MarkdownEditor extends DomNode {
     constructor();
     private onKeyDown;
     private toggleMarkdownStyle;
+    private getCaretOffset;
+    private setCaretOffset;
+    private parseMarkdownWithSpans;
+    private updateStyles;
 }
 //# sourceMappingURL=MarkdownEditor.d.ts.map
