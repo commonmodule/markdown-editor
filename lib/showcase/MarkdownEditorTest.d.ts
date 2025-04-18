5b02972f22a0d3e995704eb67b7d551fd33c393c
yjgaia <yjdev88@gmail.com>
2025-04-18 13:56:30 +0900

    feat: Implement Rich Text Editor with toolbar and editable area
    - Added RichTextEditor class with toolbar buttons for formatting (bold, italic, strikethrough, etc.)
    - Created RichTextEditableArea for text input and formatting
    - Introduced UploadImageModal for image uploads
    - Implemented dropdown menu for additional toolbar options
    - Added source maps for better debugging

--- lib/showcase/MarkdownEditorTest.d.ts
+++
@@ -1,5 +0,0 @@
-import { View } from "@commonmodule/app";
-export default class MarkdownEditorTest extends View {
-    constructor();
-}
-//# sourceMappingURL=MarkdownEditorTest.d.ts.map
--- lib/showcase/RichTextEditorTest.d.ts
+++
@@ -1,6 +0,0 @@
-import { View } from "@commonmodule/app";
-export default class RichTextEditorTest extends View {
-    private richTextEditor;
-    constructor();
-}
-//# sourceMappingURL=RichTextEditorTest.d.ts.map
